5197ac718ceff9fe7f01ab632c4786b4ff2fc4c0
Jrm1992 <jose_roberto_6@msn.com>
2023-11-18 21:10:50 -0300

    feat: models.py

--- views.py
+++ views.py
@@ -1,18 +1,22 @@
 from app import app, bot
-from flask import render_template, request, Response
+from flask import render_template, request, Response, session, flash, redirect, url_for
 import os
 from helpers import *
 from conta_tokens import *
 from resumidor import criando_resumo
+from models import *
 
 @app.route("/")
 def home():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+            return redirect(url_for('login'))
     return render_template("index.html")
+
 
 @app.route("/chat", methods = ['POST'])
 def chat():
     prompt = request.json['msg']
-    nome_do_arquivo = 'contexto_ecomart'
+    nome_do_arquivo = 'contexto_ecomart - ' + session['usuario_logado']
     contexto = ''
     if os.path.exists(nome_do_arquivo):
         contexto = carrega(nome_do_arquivo)
@@ -41,3 +45,26 @@ def limparhistorico():
         os.remove(nome_do_arquivo)
     print("Historico limpo")
     return Response("Historico limpo", mimetype = 'text/event-stream')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', proxima='/')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            return redirect(request.form['proxima'])
+    else:
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
+    
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('login'))
+
